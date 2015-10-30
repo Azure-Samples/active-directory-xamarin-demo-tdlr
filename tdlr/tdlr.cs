42e988ffbec6e24ea2045f07a755780c473b69e2
Danny Strockis <d.l.strockis@gmail.com>
2015-10-30 15:33:57 -0700

    Graph API Version 1.6

--- tdlr/tdlr.cs
+++ tdlr/tdlr.cs
@@ -13,7 +13,7 @@ namespace tdlr
 		public static string clientId = "3d8c4803-ffcd-4b2a-baec-05056abdc408";
 		public static string taskApiResourceId = "https://strockisdevtwo.onmicrosoft.com/tdlr";
 		public static string graphApiResourceId = "https://graph.windows.net";
-		public static string graphApiVersion = "1.5";
+		public static string graphApiVersion = "1.6";
 		public static string commonAuthority = "https://login.microsoftonline.com/common";
 		public static Uri redirectUri = new Uri("http://tdlr");
 
